73c465cf8f2fcc6eae2db25ab0c2c346dbc0a54e
Mike Nolan <me@michael-nolan.com>
2018-11-28 13:29:47 +0200

    Update api for new resources

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ import os
 from requests.auth import HTTPBasicAuth
 from mongoengine import connect
 
-from models import JobOpening
+from models import JobOpening, JobSeeker, Firm, Match
 from const import connect_db
 
 
@@ -17,20 +17,26 @@ CASES_URL = 'https://www.commcarehq.org/a/billy-excerpt/api/v0.5/case/'
 connect_db()
 
 class JobOpeningResource(object):
-
     def on_get(self, req, resp):
         openings = JobOpening.objects.all()
-
         resp.body = openings.to_json()
 
-        # The following line can be omitted because 200 is the default
-        # status returned by the framework, but it is included here to
-        # illustrate how this may be overridden as needed.
-        resp.status = falcon.HTTP_200
+class JobSeekerResource(object):
+    def on_get(self, req, resp):
+        openings = JobSeeker.objects.all()
+        resp.body = openings.to_json()
 
+class FirmResource(object):
+    def on_get(self, req, resp):
+        openings = Firm.objects.all()
+        resp.body = openings.to_json()
+
+class MatchResource(object):
+    def on_get(self, req, resp):
+        openings = Match.objects.all()
+        resp.body = openings.to_json()
 
 class HelloWorldResource(object):
-
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = ('hello world!')
@@ -38,6 +44,12 @@ class HelloWorldResource(object):
 api = application = falcon.API()
 
 job_opening_resource = JobOpeningResource()
+job_seeker_resource = JobSeekerResource()
+firm_resource = FirmResource()
+match_resource = MatchResource()
 hello_world_resource = HelloWorldResource()
-api.add_route('/job_opening/', job_opening_resource)
+api.add_route('/job-openings/', job_opening_resource)
+api.add_route('/job-seekers/', job_seeker_resource)
+api.add_route('/firms/', firm_resource)
+api.add_route('/matches/', match_resource)
 api.add_route('/', hello_world_resource)
