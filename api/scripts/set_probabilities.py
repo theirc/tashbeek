f3e0d21a29a0d73bbcc6b95c80a405d6b09b0a5e
Mike Nolan <me@michael-nolan.com>
2018-12-12 16:13:08 +0200

    Minor updates to probability setting
    Still not working, I think it's commcare

--- api/scripts/set_probabilities.py
+++ api/scripts/set_probabilities.py
@@ -1,10 +1,14 @@
+import os
 import requests
+from requests.auth import HTTPBasicAuth
 
 from const import connect_db
 from models import User
 
 USERS_URL = 'https://www.commcarehq.org/a/billy-excerpt/api/v0.5/user/'
 DROPBOX_URL = 'https://www.dropbox.com/s/p0jdw71vwu1quru/scores.csv?dl=1'
+COMMCARE_USERNAME = os.environ.get('COMMCARE_USERNAME')
+COMMCARE_PASSWORD = os.environ.get('COMMCARE_PASSWORD')
 
 def pretty_print_request(req):
     """
@@ -25,22 +29,29 @@ def pretty_print_request(req):
 def set_probabilities() -> None:
     r = requests.get(DROPBOX_URL)
     print('getting probabilities...')
-    probabilities = r.content.decode().split('\n')[1]
+    probabilities = r.content.decode().split('\n')[1].split(',')
     print('getting users...')
     users = User.objects.all()
     for user in users:
         if 'eso' not in user['username']:
             continue
-        user['user_data']['treatment_probabilities'] = probabilities
+        for i, prob in enumerate(probabilities):
+            user['user_data'][f"treatment_prob_{i}"] = prob
         print(f"putting data for {user['username']}...")
         user_dict = dict(user.to_mongo())
         user_dict.pop('_id')
+        print(user_dict)
 
-        req = requests.Request('PUT', USERS_URL + user.user_id, json=user_dict)
+        req = requests.Request('PUT',
+                               USERS_URL + user.user_id + '/',
+                               auth=HTTPBasicAuth(COMMCARE_USERNAME, COMMCARE_PASSWORD),
+                               json=user_dict)
         prepared = req.prepare()
         pretty_print_request(prepared)
 
-        requests.put(USERS_URL + user.user_id, json=user_dict)
+        requests.put(USERS_URL + user.user_id,
+                     auth=HTTPBasicAuth(COMMCARE_USERNAME, COMMCARE_PASSWORD),
+                     json=user_dict)
 
 if __name__ == '__main__':
     connect_db()
